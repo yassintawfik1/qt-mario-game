9ca55f15094766bf0cc2c659d39d715de79f813a
mohab2312 <mohab.badawy12@gmail.com>
2025-04-15 16:17:17 +0200

    Update Enemy.h

--- include/Enemy.h
+++ include/Enemy.h
@@ -15,22 +15,5 @@ private:
 
 #endif
 
-GameObject.h
 
-#ifndef GAMEOBJECT_H
-#define GAMEOBJECT_H
-
-#include <QGraphicsPixmapItem>
-
-
-class Player;
-
-class GameObject : public QGraphicsPixmapItem {
-public:
-    GameObject();
-    virtual ~GameObject() {}
-    
-    virtual void onCollide(Player* player);
-};
-
-#endif
+   
