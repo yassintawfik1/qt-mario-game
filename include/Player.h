7998efa5d5988753e174d0aecda1557398140a85
mohab2312 <mohab.badawy12@gmail.com>
2025-04-15 16:19:38 +0200

    Update Player.h

---
+++ include/Player.h
@@ -0,0 +1,63 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "GameObject.h"
+#include <QKeyEvent>
+
+class Level;  
+
+class Player : public GameObject {
+public:
+    Player();
+
+    int getHealth() const;
+    int getScore() const;
+    int getCoins() const;
+
+ 
+    void addScore(int points);
+    void addCoin(int count);
+    void decreaseHealth(int amount);
+    void increaseHealth(int amount);
+
+   
+    void jump();
+    void moveLeft();
+    void moveRight();
+    void stopMoving();
+
+  
+    void updateMovement();
+
+protected:
+    
+    void keyPressEvent(QKeyEvent* event) override;
+    void keyReleaseEvent(QKeyEvent* event) override;
+
+private:
+    int health;
+    int score;
+    int coins;
+    double velocityX;
+    double velocityY;
+    int jumpsRemaining;
+    bool onGround;
+    bool isGameStarted;
+};
+
+#endif
+
+PowerUp.h
+
+#ifndef POWERUP_H
+#define POWERUP_H
+
+#include "GameObject.h"
+
+class PowerUp : public GameObject {
+public:
+    PowerUp();
+    void onCollide(Player* player) override;
+};
+
+#endif
