2df411333eed3bd133cb59f89c65bd6046aef33e
mohab2312 <mohab.badawy12@gmail.com>
2025-04-15 17:43:21 +0200

    Update Player.h

--- include/Player.h
+++ include/Player.h
@@ -47,17 +47,3 @@ private:
 
 #endif
 
-PowerUp.h
-
-#ifndef POWERUP_H
-#define POWERUP_H
-
-#include "GameObject.h"
-
-class PowerUp : public GameObject {
-public:
-    PowerUp();
-    void onCollide(Player* player) override;
-};
-
-#endif
